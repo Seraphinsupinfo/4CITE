f1de5d32f3e5b2700945daca837650c777bb5c8e
Seraphinsupinfo <seraphin.dubail@supinfo.com>
2025-03-06 20:32:21 +0100

    CORS activation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
